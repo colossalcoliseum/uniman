7b9e32f692243283fbf317966af430d6fe5337f0
colossalcoliseum <alexander1tsvetanov@gmail.com>
2026-03-16 01:10:09 +0200

    added new model, controller, migration, policy,seeder, request and factory: TermPaper + added logo

---
+++ app/Http/Controllers/TermPaperController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TermPaper;
+use App\Http\Requests\StoreTermPaperRequest;
+use App\Http\Requests\UpdateTermPaperRequest;
+
+class TermPaperController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTermPaperRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(TermPaper $termPaper)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(TermPaper $termPaper)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTermPaperRequest $request, TermPaper $termPaper)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(TermPaper $termPaper)
+    {
+        //
+    }
+}
